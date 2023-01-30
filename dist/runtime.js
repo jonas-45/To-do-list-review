b4b1a827926a74d27b78349e6b861083c25e1049
jonas-45 <jonaswullah@gmail.com>
2023-01-30 15:03:00 +0000

    Remove src folder from dist directory

--- dist/runtime.js
+++ dist/runtime.js
@@ -190,7 +190,7 @@
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
 /******/ 		
-/******/ 		var chunkLoadingGlobal = self["webpackChunkto_do_list"] = self["webpackChunkto_do_list"] || [];
+/******/ 		var chunkLoadingGlobal = self["webpackChunkto_do_list_review"] = self["webpackChunkto_do_list_review"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	})();
